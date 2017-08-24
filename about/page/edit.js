d0b7c46fff1488c26afd32d7ade75313f7475d05
mix irving <whimful@gmail.com>
2017-08-24 22:03:22 +1200

    minimap working module, edit name + image

--- about/page/edit.js
+++ about/page/edit.js
@@ -1,18 +1,29 @@
 const nest = require('depnest')
+const dataurl = require('dataurl-')
+const hyperfile = require('hyperfile')
+const hypercrop = require('hypercrop')
+const hyperlightbox = require('hyperlightbox')
+const pull = require('pull-stream')
 const { h, Value, computed, map, when } = require('mutant')
+const hClassic = require('hyperscript')
 
 exports.gives = nest('about.page.edit')
 
 exports.needs = nest({
-  'about.obs.groupedValues': 'first',
-  'about.obs.image': 'first',
+  // 'about.obs.groupedValues': 'first',
+  'about.html.image': 'first',
+  // 'about.obs.image': 'first',
   'about.obs.name': 'first',
   'blob.sync.url': 'first',
+  'message.async.publish': 'first',
+  'sbot.async.addBlob': 'first',
 })
 
 const DEFAULT_LABELS = {
   name: 'Name',
-  image: 'Image',
+  avatar: 'Avatar',
+  instructionCrop: 'Click and drag to crop your avatar',
+  okay: 'Okay',
   save: 'Save',
   cancel: 'Cancel'
 }
@@ -25,39 +36,122 @@ exports.create = (api) => {
   })
 
   function editPage ({ feed, labels={} }, callback) {
-    const { name, image, save, cancel } = Object.assign({}, DEFAULT_LABELS, labels)
+    const { name, avatar, instructionCrop, okay, save, cancel } = Object.assign({}, DEFAULT_LABELS, labels)
+
+    const nameCurrent = api.about.obs.name(feed)
+    const nameNew = Value()
+
+    var avatarCurrent = api.about.html.image(feed)
+    const avatarNewData = Value()
+    const avatarToDisplay = computed(avatarNewData, data => {
+      return data 
+        ? h('img', { src: api.blob.sync.url(data.link) }) 
+        : avatarCurrent
+    })
 
     const canSave = Value(false) // computed
 
-    const names = computed(api.about.obs.groupedValues(feed, 'name'), Object.keys)
-    const nameCurrent = api.about.obs.name(feed)
-
-
-
-    const images = computed(api.about.obs.groupedValues(feed, 'image'), Object.keys)
-    const imageCurrent = api.about.obs.image(feed, 'image')
+    var lightbox = hyperlightbox()
 
     return h('PatchProfileEdit', [
-      h('section.name', [
+      h('section -name', [
         h('header', name),
-        map(names, name => h('div.name', name)),
-        '+'
+        computed(nameCurrent, name => {
+          return h('input', { value: name, 'ev-input': e => nameNew.set(e.target.value) })
+        })
       ]),
-      h('section.image', [
-        h('header', image),
-        map(images, blob => h('img', {src: api.blob.sync.url(blob)})),
-        '+'
+      h('section -avatar', [
+        lightbox,
+        h('header', avatar),
+            
+        h('div.input', [
+          avatarToDisplay,
+          hyperfile.asDataURL(dataUrlCallback),
+        ])
       ]),
       h('div.actions', [
         h('Button', { 'ev-click': () => callback(null, false) }, cancel),
         h('Button -primary', 
           { 
-            'ev-click': () => callback(null, true),
+            'ev-click': handleSave,
             className: when(canSave, '', '-disabled')
           }
           , save
         )
       ])
     ])
+
+    function handleSave () {
+      const msg = {
+        type: 'about',
+        about: feed,
+        name: nameNew(),
+        image: avatarNewData(),
+      }
+
+      if (!msg.name) delete msg.name
+      if (!msg.image || !msg.image.link) delete msg.image
+
+      api.message.async.publish(msg, (err, data) => {
+        if (err) return callback(err)
+
+        callback(null, true)
+      })
+    }
+
+    function dataUrlCallback (data) {
+      const cropCallback = (err, cropData) => {
+        if (err) throw err
+        if (!cropData) return lightbox.close()
+
+        var _data = dataurl.parse(cropData)
+        api.sbot.async.addBlob(pull.once(_data.data), (err, hash) => {
+          if (err) throw err // TODO check if this is safely caught by error catcher
+
+          avatarNewData.set({
+            link: hash,
+            size: _data.data.length,
+            type: _data.mimetype,
+            width: 512,
+            height: 512
+          })
+        })
+        lightbox.close()
+      }
+
+      const cropEl = Crop(data, cropCallback)
+      lightbox.show(cropEl)
+    }
+    
+    function Crop (data, cb) {
+      var img = h('img', {src: data})
+
+      var crop = h('div.hack')
+      waitForImg()
+
+      return crop
+
+      function waitForImg () {
+        // WEIRDNESS - if you invoke hypecrop before img is ready,
+        // the canvas instantiates and draws nothing
+        
+        if (!img.height && !img.width) {
+          return window.setTimeout(waitForImg, 100)
+        }
+
+        var canvas = hypercrop(img)
+        crop.appendChild(
+          h('PatchProfileCrop', [
+            h('header', instructionCrop),
+            canvas,
+            h('section.actions', [
+              h('Button', { 'ev-click': () => cb() }, cancel),
+              h('Button -primary', { 'ev-click': () => cb(null, canvas.selection.toDataURL()) }, okay)
+            ])
+          ])
+        )
+      }
+    }
   }
 }
+
--- styles/mcss.js
+++ styles/mcss.js
@@ -4,23 +4,131 @@ exports.gives = nest('styles.mcss')
 
 const editMcss = `
 PatchProfileEdit {
-  section.name {
-    margin-bottom: 1rem
+  padding: 1rem
+  background-color: #fff
 
-    div.name{ 
-      margin-right: .5rem
+  section {
+    margin-bottom: 2rem
+    header {
+      font-weight: 600
+      margin-bottom: 1rem
+    }
+
+    display: flex
+    flex-direction: column
+    
+    -name {
+      input { 
+        font-size: 1.2rem
+
+        padding: .5rem 
+      }
+    }
+
+    -avatar {
+      $LightBox
+
+      div.input {
+        $PatchProfileImageSize
+        position: relative
+
+        img {
+          $PatchProfileImageSize
+        }
+
+        input[type="file"] {
+          color: transparent
+
+          ::-webkit-file-upload-button {
+            visibility: hidden
+          }
+
+          ::before {
+            $PatchProfileImageSize
+            position: absolute
+            top: 0
+            display: flex
+            justify-content: center
+            align-items: center
+
+            font-size: 4rem
+            color: #fff
+            text-shadow: black 0 0 10px
+
+            transition: all .5s ease
+
+
+            cursor: pointer
+            content: '+'
+            font-size: 4rem
+
+            outline: none
+            white-space: nowrap
+            -webkit-user-select: none
+          }
+          :hover {
+            ::before {
+              text-shadow: #000 0 0 18px
+              background: rgba(0,0,0,.3)
+              box-shadow: inset rgba(0, 0, 0, 0.62) 0 0 20px
+            }
+          }
+
+          :active, :focus {
+            outline: none
+            box-shadow: none
+          }
+        }
+      }
     }
   }
-  section.image {
-    margin-bottom: 1rem
-    img {
-      max-width: 4rem
-      max-height: 4rem
-      margin-right: .5rem
-    }
+
+  div.actions {
+    margin-top: 2rem
+    display: flex
+    justify-content: space-between
+  }
+}
+
+PatchProfileCrop {
+  header {
+    font-weight: 600
+    margin-bottom: .5rem
   }
 
-  div.actions {}
+  canvas { 
+    margin: 0 auto 1rem 0
+    width: 100%
+  }
+
+  section.actions {
+    display: flex
+    justify-content: space-between
+  }
+}
+
+$PatchProfileImageSize {
+  width: 20rem
+  height: 20rem
+}
+
+$LightBox {
+  div.lightbox {
+    position: fixed;
+    left: 0px;
+    right: 0px;
+    top: 50px;
+    overflow: auto;
+    width: 780px;
+    max-width: 100%;
+    padding: 25px;
+    margin: auto;
+    z-index: 2;
+    background: #f5f5f5;
+    border: 1px solid #eee;
+    border-radius: .2em;
+  }
+
 }
 `
 
